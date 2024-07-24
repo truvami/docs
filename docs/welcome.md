f3f05ae4d62aa480a1d0aa807508e7b9e0eabec2
StefanSmile <148959525+StefanSmile@users.noreply.github.com>
2024-07-24 08:06:37 +0000

    main page redirect to docs

---
+++ docs/welcome.md
@@ -0,0 +1,7 @@
+---
+sidebar_label: Welcome
+---
+
+# Getting Started Guide
+
+### This document details the step-by-step instructions about how to set up your truvami trackers.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,8 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-import Layout from "@theme/Layout";
-import clsx from "clsx";
+import React from 'react';
+import {Redirect} from '@docusaurus/router';
 
-import styles from "./index.module.css";
+const Home = () => {
+  return <Redirect to="/docs/welcome" />;
+};
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
-}
+export default Home;
